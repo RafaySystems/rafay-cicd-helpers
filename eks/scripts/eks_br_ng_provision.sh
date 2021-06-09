0f0b89ca80b896e1fc39b517602a6ab0ded3216b
david-rafay <76073534+david-rafay@users.noreply.github.com>
2021-06-09 11:00:06 -0700

    Create eks_br_ng_provision.sh

---
+++ eks/scripts/eks_br_ng_provision.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+CLUSTER_NAME=`cat ../examples/eks-br-ng.yaml |grep name|head -1|cut -d ':' -f2`
+
+
+./rctl create cluster eks -f ../examples/eks-br-ng.yaml
+if [ $? -eq 0 ];
+then
+    echo "[+] Successfully Created cluster ${CLUSTER_NAME}"
+fi
+
+CLUSTER_STATUS_ITERATIONS=1
+CLUSTER_HEALTH_ITERATIONS=1
+CLUSTER_STATUS=`./rctl get cluster ${CLUSTER_NAME} -o json |jq '.status'|cut -d'"' -f2`
+while [ "$CLUSTER_STATUS" != "READY" ]
+do
+  sleep 60
+  if [ $CLUSTER_STATUS_ITERATIONS -ge 50 ];
+  then
+    break
+  fi
+  CLUSTER_STATUS_ITERATIONS=$((CLUSTER_STATUS_ITERATIONS+1))
+  CLUSTER_STATUS=`./rctl get cluster ${CLUSTER_NAME} -o json |jq '.status'|cut -d'"' -f2`
+  if [ $CLUSTER_STATUS == "PROVISION_FAILED" ];
+  then
+    echo -e " !! Cluster provision failed !!  "
+    echo -e " !! Exiting !!  " && exit -1
+  fi
+
+  PROVISION_STATUS=`./rctl get cluster ${CLUSTER_NAME} -o json |jq '.provision.status' |cut -d'"' -f2`
+
+  if [ $PROVISION_STATUS == "INFRA_CREATION_FAILED" ];
+  then
+    echo -e " !! Cluster provision failed !!  "
+    echo -e " !! Exiting !!  " && exit -1
+  fi
+
+  if [ $PROVISION_STATUS == "BOOTSTRAP_CREATION_FAILED" ];
+  then
+    echo -e " !! Cluster provision failed !!  "
+    echo -e " !! Exiting !!  " && exit -1
+  fi
+
+  PROVISION_STATE=`./rctl get cluster ${CLUSTER_NAME} -o json | jq '.provision.running_state' |cut -d'"' -f2`
+
+  echo "$PROVISION_STATE in progress"
+done
+if [ $CLUSTER_STATUS != "READY" ];
+then
+    echo -e " !! Cluster provision failed !!  "
+    echo -e " !! Exiting !!  " && exit -1
+fi
+if [ $CLUSTER_STATUS == "READY" ];
+then
+    echo "[+] Cluster Provisioned Successfully waiting for it to be healthy"
+    CLUSTER_HEALTH=`./rctl get cluster ${CLUSTER_NAME} -o json | jq '.health' |cut -d'"' -f2`
+    while [ "$CLUSTER_HEALTH" != 1 ]
+    do
+      echo "Iteration-${CLUSTER_HEALTH_ITERATIONS} : Waiting 60 seconds for cluster to be healthy..."
+      sleep 60
+      if [ $CLUSTER_HEALTH_ITERATIONS -ge 15 ];
+      then
+        break
+      fi
+      CLUSTER_HEALTH_ITERATIONS=$((CLUSTER_HEALTH_ITERATIONS+1))
+      CLUSTER_HEALTH=`./rctl get cluster ${CLUSTER_NAME} -o json | jq '.health' |cut -d'"' -f2`
+    done
+fi
+
+if [[ $CLUSTER_HEALTH == 0 ]];
+then
+    echo -e " !! Cluster is not healthy !!  "
+    echo -e " !! Exiting !!  " && exit -1
+fi
+if [[ $CLUSTER_HEALTH == 1 ]];
+then
+    echo "[+] Cluster Provisioned Successfully and is Healthy"
+fi
